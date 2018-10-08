43681e1f0bf787890306fe088eeabe339b9843c2
Jeffin Francis <francisjeffin07@gmail.com>
2018-10-08 20:14:17 +0530

    Example for matplotlib

---
+++ Python 3.x/2Dplot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plot
+#if you want to plot two variables and see the graph
+#let x and y be two variables
+x=[1,2,3,4,5]
+y=[6,7,8,9,10]
+plot.plot(x,y)
+plot.xlabel("X")
+plot.ylabel("Y")
+plot.show()
+
